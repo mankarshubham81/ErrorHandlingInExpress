510d25aa5cd242d35d995450194977c151255b65
Shubham Mankar <mankarshubham81@gmail.com>
2024-03-16 22:59:37 +0530

    In DB user is already existed or not

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,11 +2,12 @@ import CustomErrorHandler from "../utils/errorHandler.js"
 import { User } from "../model/user.js"
 
 export const newUser = async (req,res, next) => {
-    const userExist = false;
+try {
+    const userExist = User.findOne({email: "sam@gmail.com"});
     if(userExist){
         return next(new CustomErrorHandler("User already Exist", 400))
     }
-
+    
     await User.create({
         name: "Sam",
         email: "sam@gmail.com"
@@ -15,5 +16,9 @@ export const newUser = async (req,res, next) => {
     res.status(201).json({
        message: "User created succesfully"
     })
+} catch (error) {
+    return next(new CustomErrorHandler(error))
+}
+
     
 }
